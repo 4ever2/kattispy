e19391bcbc98d078b42069079c771ccce39d4e87
Eske <eskehoy@gmail.com>
2018-10-01 22:15:01 +0200

    Added input parameter test. for pointing to where sample data is stored. Added code to read and evaluate all sample data read.

--- Main.py
+++ Main.py
@@ -1,16 +1,27 @@
 from argparse import ArgumentParser
 import subprocess
 from subprocess import Popen, PIPE
+from os import listdir
+from os.path import isfile, join
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-prog', dest="prog", required=True)
+    parser.add_argument('-test', dest="test", required=True)
     args = parser.parse_args()
     print('vars args', vars(args))
 
-    f = open("test.in")
+    infiles = [join(args.test, f) for f in listdir(args.test) if isfile(join(args.test, f)) & f.endswith(".in")]
+    ansfiles = [join(args.test, f) for f in listdir(args.test) if isfile(join(args.test, f)) & f.endswith(".ans")]
 
-    print(Popen("python " + args.prog, stdout=PIPE, stdin=f).stdout.read())
-    f.seek(0)
-    print(f.read())
+    #print(infiles)
+    #print(ansfiles)
+
+    out = []
+
+    for i in infiles:
+        f = open(i)
+        out.append(Popen("python " + args.prog, stdout=PIPE, stdin=f).stdout.read())
+    
+    print(out)
